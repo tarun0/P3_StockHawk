8b6646d0f25e098cbd1bdd19360598bc6cae2589
tarun0 <tarun.mudgal34@gmail.com>
2016-04-17 01:03:25 +0530

    Implemented Activity state storing and restoring

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailsChartActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailsChartActivity.java
@@ -18,7 +18,6 @@ import com.sam_chordas.android.stockhawk.model.RequestModel;
 import com.sam_chordas.android.stockhawk.service.RetrieveHistoryService;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
@@ -28,15 +27,15 @@ import retrofit.client.Response;
 public class DetailsChartActivity extends Activity {
     Intent intent;
     String query;
-    List<Float> prices;
-    List<String> dates;
+    ArrayList<Float> prices;
+    ArrayList<String> dates;
     BarChart chart;
     BarDataSet barDataSet;
     BarData barData;
     ArrayList<BarEntry> entries;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_chart);
         chart = (BarChart) findViewById(R.id.barchart);
@@ -48,75 +47,127 @@ public class DetailsChartActivity extends Activity {
         catch (NullPointerException e) {
             e.printStackTrace();
         }
-        final ProgressDialog dialog = new ProgressDialog(this);
-        dialog.setMessage(getString(R.string.progress_dialog_message));
-        dialog.show();
-        RestAdapter builder = new RestAdapter.Builder()
-                .setLogLevel(RestAdapter.LogLevel.FULL)
-                .setEndpoint("https://query.yahooapis.com/v1/public")
-                .build();
 
-        query = "select * from yahoo.finance.historicaldata where symbol = \'" +
-                intent.getStringExtra("symbol") +
-                "\' and startDate = \'2009-09-11\' and endDate = \'2010-03-10\'";
+        //if savedInstanceState is null
+        if (savedInstanceState == null) {
+            final ProgressDialog dialog = new ProgressDialog(this);
+            dialog.setMessage(getString(R.string.progress_dialog_message));
+            dialog.show();
+            RestAdapter builder = new RestAdapter.Builder()
+                    .setLogLevel(RestAdapter.LogLevel.FULL)
+                    .setEndpoint("https://query.yahooapis.com/v1/public")
+                    .build();
 
-        RetrieveHistoryService retrieveHistoryService = builder.create(RetrieveHistoryService.class);
-        retrieveHistoryService.getHistory(query, new Callback<RequestModel>() {
-            @Override
-            public void success(RequestModel retrievedResponse, Response response) {
-                dialog.dismiss();
-                if (retrievedResponse.getQuery().getCount() > 0) {
+            query = "select * from yahoo.finance.historicaldata where symbol = \'" +
+                    intent.getStringExtra("symbol") +
+                    "\' and startDate = \'2009-09-11\' and endDate = \'2010-03-10\'";
 
-                    Toast.makeText(getApplicationContext(), R.string.pinch_to_zoom, Toast.LENGTH_SHORT).show();
-                    int total = retrievedResponse.getQuery().getCount();
-                    prices = new ArrayList<>();
-                    dates = new ArrayList<String>();
+            RetrieveHistoryService retrieveHistoryService = builder.create(RetrieveHistoryService.class);
+            retrieveHistoryService.getHistory(query, new Callback<RequestModel>() {
+                @Override
+                public void success(RequestModel retrievedResponse, Response response) {
+                    dialog.dismiss();
+                    if (retrievedResponse.getQuery().getCount() > 0) {
 
-                    ArrayList<Quote> quoteList = retrievedResponse.getQuery().getResults().getQuote();
-                    entries = new ArrayList<BarEntry>();
+                        Toast.makeText(getApplicationContext(), R.string.pinch_to_zoom, Toast.LENGTH_SHORT).show();
+                        int total = retrievedResponse.getQuery().getCount();
+                        prices = new ArrayList<>();
+                        dates = new ArrayList<String>();
 
-                    for (int i = 0; i < total; i++) {
-                        prices.add(quoteList.get(i).getClose());
-                        dates.add(quoteList.get(i).getDate());
-                        entries.add(new BarEntry(prices.get(i), i));
+                        ArrayList<Quote> quoteList = retrievedResponse.getQuery().getResults().getQuote();
+                        entries = new ArrayList<BarEntry>();
+
+                        for (int i = 0; i < total; i++) {
+                            prices.add(quoteList.get(i).getClose());
+                            dates.add(quoteList.get(i).getDate());
+                            entries.add(new BarEntry(prices.get(i), i));
+                        }
+
+                        /*barDataSet = new BarDataSet(entries, "Close Price");
+                        barData = new BarData(dates, barDataSet);*/
+
+
+
+
+
+
+                        /*chart.setData(barData);
+                        chart.setDescription("Close price vs Date");
+                        chart.animateY(1500);
+                        chart.invalidate();*/
+                        makeChart(dates, prices);
+
+                    } else {
+                        AlertDialog.Builder alertDialog = new AlertDialog.Builder(DetailsChartActivity.this);
+                        alertDialog.setTitle(R.string.message).setMessage(R.string.no_history_data_available).setCancelable(false)
+                                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Intent intent = new Intent(getApplicationContext(), MyStocksActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    }
+                                }).show();
                     }
-
-                    barDataSet = new BarDataSet(entries, "Close Price");
-                    barData = new BarData(dates, barDataSet);
-
-                    chart.setData(barData);
-                    chart.setDescription("Close price vs Date");
-                    chart.animateY(1500);
-                    chart.invalidate();
                 }
-                else {
-                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(DetailsChartActivity.this);
-                    alertDialog.setTitle(R.string.message).setMessage(R.string.no_history_data_available).setCancelable(false)
-                           .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                               @Override
-                               public void onClick(DialogInterface dialogInterface, int i) {
-                                   Intent intent = new Intent(getApplicationContext(), MyStocksActivity.class);
-                                   intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                   startActivity(intent);
-                               }
-                           }).show();
-                }
-            }
 
-            @Override
-            public void failure(RetrofitError error) {
-                dialog.dismiss();
-                AlertDialog.Builder alert = new AlertDialog.Builder(DetailsChartActivity.this);
-                alert.setTitle(R.string.message).setMessage(R.string.retrieve_chart_network_issue);
-                alert.setCancelable(false).setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent intent = new Intent(getApplicationContext(), MyStocksActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        startActivity(intent);
-                    }
-                }).show();
-            }
-        });
+                @Override
+                public void failure(RetrofitError error) {
+                    dialog.dismiss();
+                    AlertDialog.Builder alert = new AlertDialog.Builder(DetailsChartActivity.this);
+                    alert.setTitle(R.string.message).setMessage(R.string.retrieve_chart_network_issue);
+                    alert.setCancelable(false).setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent intent = new Intent(getApplicationContext(), MyStocksActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        }
+                    }).show();
+                }
+            });
+        }
+        else {
+            //make chart
+            dates = savedInstanceState.getStringArrayList("dates_array");
+            prices = floatToArrayList(savedInstanceState.getFloatArray("prices_array"));
+            makeChart(dates, prices);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList("dates_array", dates);
+        outState.putFloatArray("prices_array", arrayListToArray(prices));
+    }
+
+    void makeChart (ArrayList<String> date, ArrayList<Float> price) {
+        entries = new ArrayList<BarEntry>();
+        for (int i=0; i<date.size(); i++) {
+            entries.add(new BarEntry(price.get(i), i));
+        }
+        barDataSet = new BarDataSet(entries, "Close Price");
+        barData = new BarData(date, barDataSet);
+        chart.setData(barData);
+        chart.setDescription("Close price vs Date");
+        chart.animateY(1500);
+        chart.invalidate();
+    }
+
+    float[] arrayListToArray(ArrayList<Float> list) {
+        float[] array = new float[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+    }
+
+    ArrayList<Float> floatToArrayList (float[] array) {
+        ArrayList<Float> list = new ArrayList<>();
+        for (int i=0; i<array.length; i++) {
+            list.add(array[i]);
+        }
+        return list;
     }
 }
